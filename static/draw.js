bcb7bab0983e9e27e2c95adc0928282159ad34cf
meanlessnessener <mr.sansanich05@mail.ru>
2024-08-25 03:20:11 +0500

    pretty graphics

---
+++ static/draw.js
@@ -0,0 +1,220 @@
+// parsing
+__readLineIndexer = 0
+function readLine(logs) {
+    if (__readLineIndexer < logs.length) {
+        return logs[__readLineIndexer++]
+    }
+    return ""
+}
+
+function parseLogs(logs) {
+    __readLineIndexer = 0;
+    var res = []
+    var fld = []
+
+    fldHeight = Number(readLine(logs).split(" ")[1])
+    for (i = 0; i < fldHeight; i++) {
+        fld.push(readLine(logs))
+    }
+
+    for (turn = 0; ; turn++) {
+        line = readLine(logs)
+        cmd = line.split(" ")
+
+        if (cmd[0] == "win") {
+            res.push({
+                type: "win",
+                who: cmd[1],
+            })
+            break
+        } else if (cmd[0] == "data") {
+            p1 = getRCD(readLine(logs))
+            p2 = getRCD(readLine(logs))
+
+            bulletsNum = Number(readLine(logs))
+            bullets = []
+
+            for (i = 0; i < bulletsNum; i++) {
+                bullets.push(getRCD(readLine(logs)))
+            }
+
+            if (turn % 2 == 0) {
+                continue
+            }
+
+            res.push({
+                type: "move",
+                p: [p2, p1],
+                bullets: bullets,
+            })
+        }
+    }
+
+    return [fld, res]
+}
+
+// init
+var cnv = document.getElementById("canvas")
+var ctx = cnv.getContext("2d")
+
+var [field, moves] = parseLogs(gameLog)
+
+const cellsVer = field.length
+const cellsHor = field[0].length
+
+cnv.width = 400
+cnv.height = 400
+
+cnv.width += cellsHor - (cnv.width % cellsHor)
+cnv.height += cellsVer - (cnv.height % cellsVer)
+const cellSize = Math.min(cnv.width / cellsHor, cnv.height / cellsVer)
+
+///////////////////////////////////////
+
+images = {
+    tank: [{
+        up: new Image(),
+        down: new Image(),
+        left: new Image(),
+        right: new Image(),
+    }, {
+        up: new Image(),
+        down: new Image(),
+        left: new Image(),
+        right: new Image(),
+    }],
+    bullet: {
+        up: new Image(),
+        down: new Image(),
+        left: new Image(),
+        right: new Image(),
+    },
+    wall: new Image(),
+    boom: {
+        1: new Image(),
+        2: new Image(),
+        3: new Image(),
+    }
+}
+
+// load pics
+images.wall.src = "src/sprites/wall.png"
+images.boom.src = "src/sprites/wall.png"
+for (i = 1; i <= 3; i++) {
+    images.boom[i].src = "src/sprites/bullet_explosion_" + i + ".png"
+}
+for (dir in images.bullet) {
+    for (i = 0; i < 2; i++) {
+        images.tank[i][dir].src = "src/sprites/tank_power_" + dir + "_c0_t1" + ["", "_f"][i] + ".png"
+    }
+    images.bullet[dir].src = "src/sprites/bullet_" + dir + ".png"
+}
+
+// init
+ctx.fillStyle = "black"
+ctx.fillRect(0, 0, cnv.width, cnv.height)
+document.getElementById("status").innerText = ""
+
+
+// funcs
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function getCoords(r, c) {
+    return [r * cellSize, c * cellSize]
+}
+
+function drawWall(r, c) {
+    [x, y] = getCoords(r, c)
+    ctx.drawImage(images.wall, x, y, cellSize, cellSize)
+}
+
+function drawNothing(r, c) {
+    [x, y] = getCoords(r, c)
+    ctx.fillStyle = "black"
+    ctx.fillRect(x, y, cellSize, cellSize)
+}
+
+function drawTank(obj, p) {
+    [x, y] = getCoords(obj.r, obj.c)
+    p -= 1
+    ctx.drawImage(images.tank[p][obj.d], x, y, cellSize, cellSize)
+}
+
+function drawBullet(obj) {
+    [x, y] = getCoords(obj.r, obj.c)
+    let bulletSize = images.bullet[obj.d].width
+    ctx.drawImage(images.bullet[obj.d], x + cellSize * 0.5 - bulletSize * 0.5, y + cellSize * 0.5 - bulletSize * 0.5, bulletSize, bulletSize)
+}
+
+async function drawBoom(r, c, frame = 1) {
+    [x, y] = getCoords(r, c)
+    if (frame > 3) {
+        ctx.fillStyle = "black"
+        ctx.fillRect(x, y, cellSize, cellSize)
+        return
+    }
+    console.log(r, c, frame)
+    ctx.drawImage(images.boom[frame], x, y, cellSize, cellSize)
+    setTimeout(drawBoom, 75, r, c, frame + 1)
+}
+
+function drawField() {
+    for (i = 0; i < cellsVer; i++) {
+        for (j = 0; j < cellsHor; j++) {
+            switch (field[i][j]) {
+                case '#': {
+                    drawWall(j, i)
+                    break
+                }
+                case '.': {
+                    drawNothing(j, i)
+                    break
+                }
+            }
+        }
+    }
+}
+
+function getRCD(line) {
+    data = line.split(" ")
+    res = {
+        r: Number(data[0]),
+        c: Number(data[1]),
+        d: data[2],
+    }
+    return res
+}
+
+
+async function simulate() {
+    document.getElementById("status").innerText = "Играем..."
+
+    for (turn = 0; turn < moves.length; turn++) {
+        console.log(moves[turn])
+
+        if (moves[turn].type == "win") {
+            let winner = moves[turn].who - 1
+            let looser = 1 - winner
+            document.getElementById("status").innerText = ["Белый", "Красный"][winner] + " победил!"
+            if (turn > 0) {
+                drawBoom(moves[turn-1].p[looser].r, moves[turn-1].p[looser].c)
+            }
+            break
+        }
+
+        drawField()
+        drawTank(moves[turn].p[0], 1)
+        drawTank(moves[turn].p[1], 2)
+
+        for (i = 0; i < moves[turn].bullets.length; i++) {
+            drawBullet(moves[turn].bullets[i])
+        }
+
+        await sleep(500);
+    }
+}
+
+var startButton = document.getElementById("start-button")
+startButton.addEventListener("click", simulate);
